5bc82872604684f9d2799bfcc2a968aa7ff39ee3
Peter Smith <shipwrecksoftware@live.com>
2023-01-08 14:51:20 -0800

    Timing is correct now; graph is for just 10 packets, which is seemingly not enough.

--- SimpleWiFiAnalyzer/Assets/4AddingTheLatencyTest.md
+++ SimpleWiFiAnalyzer/Assets/4AddingTheLatencyTest.md
@@ -4,9 +4,28 @@
 
 I discovered, while looking for something totally different on the FCC.GOV website, that the American (USA) FCC (Federal Communications Commission) has created a speed and latency test for broadband and documented (roughtly) the protocol involved!
 
+## Latency Timing
+
+From the FCC Technical Description, page 5:
+
+_Latency_: Measures the average round-trip time in milliseconds of up to 200UDP(User Datagram Protocol)data packets that are acknowledged as received individually within 2 seconds or are recorded as lost.  The packets are sent over a fixed 5-second time interval.No warm-up period is applied to latency.
+
+Packets are sent for 5 seconds with a 2 second wait for packets, sending a maximum of 200 packets.
+Algorithn is to send a packet and wait up to 2 seconds for the reply, sleeping 
+
+
 ## UWP Network hints
 
 
+## Errata
+
+**FCC Technical Description**
+
+Page 9, Latency and Packet loss. The packets are described as being 160 bytes. According to the Samknows Github android code, the packets are actually 16 bytes. 60 bytes does work. Note that the reply packets are 16 bytes long regardless of the sent packet size.
+
+Page 9: The technical description has each packet having as 8-byte sequence number and an 8-byte timestamp. The Github code instead has each packet as contains four four-byte (big endian) integers: a sequence number, a Unix seconds count, a milliseconds count, and a "magic" number
+
+Page 9: The pseudo-code has the port being set to 5000. The Android code uses port 6000. AS of 2023-01-08, Port 5000 does not return any packets (seattle).
 
 ## Handy links.
 
--- SpeedTests/FccSpeedTest2022.cs
+++ SpeedTests/FccSpeedTest2022.cs
@@ -10,6 +10,7 @@ using Windows.Networking;
 using Windows.Networking.Sockets;
 using Windows.Security.Cryptography;
 using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls.Primitives;
 
 namespace SpeedTests
 {
@@ -90,6 +91,7 @@ namespace SpeedTests
             }
             public void SetEndTime(long s, long ms)
             {
+                HaveEndTime = true;
                 EndTime = DateTimeOffset.Now;
                 var stunix = DateTimeOffset.FromUnixTimeMilliseconds(s * 1000 + ms);
                 ServerTime = stunix.ToOffset(EndTime.Offset);
@@ -97,6 +99,7 @@ namespace SpeedTests
             public int Id { get; set; }
             public DateTimeOffset StartTime { get; internal set; }
             public DateTimeOffset EndTime { get; internal set; }
+            public bool HaveEndTime { get; internal set; } = false;
             public DateTimeOffset ServerTime { get; internal set; }
             public double TimeInSeconds { get { if (EndTime == null) return -1; return EndTime.Subtract(StartTime).TotalSeconds; } }
             public double ToServerInSeconds { get { if (ServerTime == null) return -1; return ServerTime.Subtract(StartTime).TotalSeconds; } }
@@ -108,7 +111,10 @@ namespace SpeedTests
                 long unixMilliseconds = timestamp.ToUnixTimeMilliseconds();
                 int seconds = (int)((unixMilliseconds / 1000) & 0xFFFFFFFF);
                 int milliseconds = (int)(unixMilliseconds % 1000);
-                var packet = new byte[16];
+                var packet = new byte[160];
+                //ERRATA: FCC Technical description, page 9 says the packets are 160 bytes. The
+                // Github code says 16 bytes.
+                // 2023-01-08: 16 bytes works perfectly, as does 160 bytes. The return bytes are 16 bytes long.
 
                 WriteInt32(packet,  0, sequence);
                 WriteInt32(packet,  4, seconds);
@@ -131,8 +137,13 @@ namespace SpeedTests
         /// </summary>
 
         /// <returns></returns>
-        public async Task<LatencyTestResults> LatencyTestAsync(List<ISetStatistics> uxlist, HostName server, string port = "6000", double interPacketTimeInMilliseconds = 500, double delayTimoutInSeconds = 2.0, int nDatagrams = 200, double maxTimeInSeconds = 5.0)
+        public async Task<LatencyTestResults> OrigLatencyTestAsync(List<ISetStatistics> uxlist, HostName server, string port = "6000", double interPacketTimeInMilliseconds = 500, double delayTimoutInSeconds = 2.0, int nDatagrams = 200, double maxTimeInSeconds = 5.0)
         {
+            // ERRATA: the default port here is "6000" (UWP calls these "services" which is the
+            // RFC-compliant way of thinking about them. But the FCC Technical doc says the port 
+            // should be 5000.
+            // 2023-01-08: Port 6000 (Android code) works perfectly. Port 5000 does not work.
+
             nDatagrams = 20; // TODO: don't send too many yet!
             if (server == null)
             {
@@ -215,7 +226,118 @@ namespace SpeedTests
             int id = dr.ReadInt32();
             int s = dr.ReadInt32();
             int ms = dr.ReadInt32();
+            int serverMagic = dr.ReadInt32();
             IndividualTests[id].SetEndTime(s, ms);
         }
+
+        public async Task<LatencyTestResults> LatencyTestAsync(List<ISetStatistics> uxlist, HostName server, string port = "6000", 
+            double interPacketTimeInMilliseconds = 500,
+            double delayTimoutInSeconds = 2.0, 
+            int maxDatagrams = 200, 
+            double maxTimeInSeconds = 5.0)
+        {
+            // ERRATA: the default port here is "6000" (UWP calls these "services" which is the
+            // RFC-compliant way of thinking about them. But the FCC Technical doc says the port 
+            // should be 5000.
+            // 2023-01-08: Port 6000 (Android code) works perfectly. Port 5000 does not work.
+
+            if (server == null) // TODO: pick server correctly.
+            {
+                server = new HostName(Servers[0]);
+            }
+            IndividualTests = new LatencyTestSingle[maxDatagrams];
+
+            var retval = new LatencyTestResults(server, port);
+            LTR = retval;
+
+            using (var socket = new DatagramSocket())
+            {
+                try
+                {
+                    socket.MessageReceived += Socket_MessageReceived;
+                    await socket.ConnectAsync(server, port);
+                }
+                catch (Exception ex)
+                {
+                    retval.Error = $"Unable to connect: server={server} port={port} error={ex.Message}";
+                    return retval;
+                }
+                var outputStream = socket.OutputStream;
+                var writer = new DataWriter(outputStream);
+                uint nbytes = 0;
+
+                bool keepGoing = true;
+                var endTime = DateTimeOffset.UtcNow.AddSeconds(maxTimeInSeconds);
+                var uiUpdateTime = DateTimeOffset.UtcNow.AddMilliseconds(250); // Update the UX every ZZZ milliseconds.
+                while (keepGoing)
+                {
+                    int i = retval.NSent; // get an index number; it will be used a lot
+                    retval.NSent++;
+                    IndividualTests[i] = new LatencyTestSingle(i);
+                    var packet = IndividualTests[i].Packet;
+                    writer.WriteBytes(packet);
+                    var result = await writer.StoreAsync();
+                    nbytes += result;
+
+                    // Wait for the reply
+                    var packetEndTime = DateTimeOffset.UtcNow.AddSeconds(delayTimoutInSeconds);
+                    while (!IndividualTests[i].HaveEndTime
+                        && DateTimeOffset.UtcNow < packetEndTime)
+                    {
+                        await Task.Delay(5); // NOTE: seems like an OK value?
+                        if (DateTimeOffset.UtcNow > uiUpdateTime)
+                        {
+                            retval.Calculate(IndividualTests);
+                            foreach (var ux in uxlist)
+                            {
+                                ux.SetStatistics(retval.SpeedStatistics);
+                            }
+                            uiUpdateTime = DateTimeOffset.UtcNow.AddMilliseconds(250);
+                        }
+                    }
+
+                    var pauseTimeInMilliseconds = interPacketTimeInMilliseconds;
+                    if (IndividualTests[i].HaveEndTime)
+                    {
+                        var actualEndTime = IndividualTests[i].EndTime;
+                        var startTime = IndividualTests[i].StartTime;
+                        var packetTime = actualEndTime.Subtract(startTime);
+                        pauseTimeInMilliseconds -= packetTime.TotalMilliseconds;
+                    }
+                    else
+                    {
+                        // In reality, this is always going to be negative.
+                        pauseTimeInMilliseconds = interPacketTimeInMilliseconds - (delayTimoutInSeconds * 1_000.0);
+                    }
+                    if (pauseTimeInMilliseconds > 0)
+                    {
+                        await Task.Delay((int)pauseTimeInMilliseconds);
+                    }
+
+                    // TODO: slight difference in results: original code will not allow for
+                    // packets that straggle in while we're waiting for a different packet.
+
+                    // Check for end conditions.
+                    if (DateTimeOffset.UtcNow > endTime)
+                    {
+                        keepGoing= false;
+                    }
+                    if (retval.NSent >= maxDatagrams)
+                    {
+                        keepGoing = false;
+                    }
+                };
+            } // Close the socket.
+
+            // Final stat calculation and UX update
+            retval.Calculate(IndividualTests);
+            foreach (var ux in uxlist)
+            {
+                ux.SetStatistics(retval.SpeedStatistics);
+            }
+
+            return retval;
+        }
+
     }
 }
