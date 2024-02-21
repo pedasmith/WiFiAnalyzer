8731a1df8bde1a80e480e5f0fd56d0e1ad53d396
toomre <toomre@live.com>
2024-02-21 09:09:56 -0800

    Works on the latest Windows now! Woot, woot!

--- NetworkSetup/MainPage.xaml.cs
+++ NetworkSetup/MainPage.xaml.cs
@@ -5,8 +5,10 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System.Display;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,6 +29,19 @@ namespace NetworkSetup
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void Log(string str)
+        {
+            uiLog.Text += str + "\n";
+        }
+
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            Log($"NOTE: Location request status:{accessStatus}");
         }
 
         public async Task NavigateToMeCard(MeCardRaw mecard)
@@ -35,7 +50,7 @@ namespace NetworkSetup
             {
                 case "NETWORKSETUP":
                     {
-                        // Example: networksetup:context:hotspot;action:start;ssid:myhotspot;p:password;;
+                        // Example: networksetup:context:hotspot;action:start;S:myhotspot;P:password;band:5;auth:WPA3;
                         // potential actions includes start stop report
                         var context = mecard.GetFieldValue("context", "unknown");
                         var action = mecard.GetFieldValue("action", "unknown");
@@ -49,6 +64,19 @@ namespace NetworkSetup
                     }
                     break;
             }
+        }
+
+        DisplayRequest CurrDisplayRequest = null;
+        private void OnCheckScreenOff(object sender, RoutedEventArgs e)
+        {
+            CurrDisplayRequest.RequestRelease();
+        }
+        private void OnCheckScreenOn(object sender, RoutedEventArgs e)
+        {
+            if (CurrDisplayRequest == null) {
+                CurrDisplayRequest = new Windows.System.Display.DisplayRequest();
+            }
+            CurrDisplayRequest.RequestActive();
         }
     }
 }
